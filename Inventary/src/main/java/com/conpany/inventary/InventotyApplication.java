dd15af28bbff510cf85e99386f395e0057186981
= <sdiego-2000@hotmail.com>
2023-01-21 15:45:28 -0500

    proyecto base de control de inventario

---
+++ Inventary/src/main/java/com/conpany/inventary/InventotyApplication.java
@@ -0,0 +1,13 @@
+package com.conpany.inventary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventotyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventotyApplication.class, args);
+	}
+
+}
---
+++ Inventary/src/test/java/com/conpany/inventary/InventotyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conpany.inventary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventotyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
